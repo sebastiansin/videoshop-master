a1307c00546679554b3f5a962aa3f9f49b85461f
sebastiansin <37374057+sebastiansin@users.noreply.github.com>
2018-10-22 21:37:41 +0200

    Initial commit

---
+++ src/main/java/videoshop/customer/Rabatt.java
@@ -0,0 +1,10 @@
+package videoshop.customer;
+
+public class Rabatt {
+	
+	//@GetMapping("/customers")
+//	@PreAuthorize("hasRole('ROLE_BOSS')")
+	
+//	int amount = rabatt <= 0 || rabatt > 100 ? 1 : rabatt;
+
+}
